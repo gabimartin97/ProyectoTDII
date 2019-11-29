ddba1c98a51e61d1d56214ff564d62accb1fbfb6
gabimartin97 <57768880+gabimartin97@users.noreply.github.com>
2019-11-29 13:09:33 -0300

    Update ElChoque.c

--- ElChoque.c
+++ ElChoque.c
@@ -12,6 +12,7 @@ void menuchoque();
 int delayChoque=0;
 /*....................................main............................*/
 void ElChoque(){
+	int i;
 	pioInit();    //No olvidarse de pioInit
 	for(i=0;i<8;i++) pinMode(leds[i],OUTPUT); //Declaro leds como salida
 	menuchoque();
